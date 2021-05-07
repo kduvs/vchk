4c046ae59af3bca70f282cc4f55e7de31fadc234
Vladislav Sanzhiev <vernitemnemoi2007@bk.ru>
2021-05-07 21:53:26 +0800

    qr-for-php

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -13,6 +13,7 @@ use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
 use yii\data\ActiveDataProvider;
 use yii\helpers\ArrayHelper;
+use Da\QrCode\QrCode;
 use Yii;
 
 class UsersController extends \yii\web\Controller
@@ -31,22 +32,9 @@ class UsersController extends \yii\web\Controller
     public function actionNotifications($id = null)
     {
         $model = new LogIssuing();
-        //print_r(Yii::$app->request->post('LogIssuing'));
         if(Yii::$app->request->post('LogIssuing')){
-            //print_r(Yii::$app->request->post('LogIssuing')['response']);
-            //print_r(Yii::$app->request->post());
-            //exit(0);
-
-            //$model = LogIssuing::findOne($id);
-            //if($model->load(Yii::$app->request->post())) {
-            //    $model->save();
-            //}
             $model->load(Yii::$app->request->post());
-            //print_r($model);
-            //exit(0);
-            //print_r(Yii::$app->request->post(LogIssuing::className()));
-            //echo '<br>';
-            //print_r(Yii::$app->request->post(LogIssuing::className())[LogIssuing::$primaryKey]);
+            
             $model = LogIssuing::findOne(Yii::$app->request->post('LogIssuing')['id']);
             if($model->load(Yii::$app->request->post())) {
                 $model->save();
@@ -54,9 +42,7 @@ class UsersController extends \yii\web\Controller
             else {
                 print_r(Yii::$app->request->post());
             }
-            
         }
-        
         
         if(Yii::$app->request->post('submit') === 'positive') {
             print_r(Yii::$app->request->post());
@@ -69,63 +55,35 @@ class UsersController extends \yii\web\Controller
         return $this->render('notifications', [
             'sentSearchModel' => $sentSearchModel,
             'inboxSearchModel' => $inboxSearchModel,
-        ]);
-        
+        ]);   
     }
-
-    // public function actionInbox()
-    // {
-    //     $searchModel = new LogIssuingSearch();
-    //     $searchModel->owner_id = Yii::$app->user->identity->owner_id;
-    //     $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-    //     return $this->render('notifications', [
-    //         'dataProvider' => $dataProvider,
-    //         'searchModel' => $searchModel
-    //     ]);
-    // }
-
-    // public function actionSent()
-    // {
-    //     $searchModel = new LogIssuingSearch();
-    //     $serachModel->taker_id = Yii::$app->user->identity->id;
-    //     $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-    //     return $this->render('notifications', [
-    //         'dataProvider' => $dataProvider,
-    //         'searchModel' => $searchModel
-    //     ]);
-    // }
 
     public function actionProfile($username = null) //переделать под правило без использования getColumn
     {
-        //$books = Books::find()->where(['owner_id' => Yii::$app->user->identity->getId()])->all();
-        $searchModel = new BooksSearch();
-        
-        //Yii::$app->request->queryParams
         if(!isset($username)){
             if(!Yii::$app->user->isGuest){
-                //$model = User::findOne(Yii::$app->user->identity->getId());
                 $model = Yii::$app->user->identity;
-            } else {
-                throw new ForbiddenHttpException;
-            }
+                $qrCode = (new QrCode('https://vk.com')) //пока пусть будет просто айди $model->id
+                    ->setSize(250)
+                    ->setMargin(5)
+                    ->useForegroundColor(51, 153, 255);
+
+                // $qrCode->writeFile(__DIR__ . '/code.png');
+
+                // header('Content-Type: '.$qrCode->getContentType());
+                // echo $qrCode->writeString();
+                $q = '<img src="' . $qrCode->writeDataUri() . '">';
+
+            } else throw new NotFoundHttpException('The requested page does not exist.');
+
         } else {
             $model = $this->findModel($username);
-            $RoleByUsername = current(ArrayHelper::getColumn(Yii::$app->authManager->getRolesByUser($model->id), 'name'));
-            if($RoleByUsername == 'admin'){
-                $CurrentRole = current(ArrayHelper::getColumn(Yii::$app->authManager->getRolesByUser(Yii::$app->user->id), 'name'));
-                if($CurrentRole != $RoleByUsername){
-                    throw new ForbiddenHttpException;
-                }
-            }
+            $q = '';
         }
-
-        $searchModel->owner_id = $model->owner_id;
-        $books = $searchModel->search(Yii::$app->request->queryParams);
         
         return $this->render('profile', [
-            'books' => $books,
             'model' => $model,
-            'substr' => !isset($username) ? ' ваших ' : ' ',
+            'q' => $q,
         ]);
     }
 
--- views/users/profile.php
+++ views/users/profile.php
@@ -19,78 +19,9 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
-    <h3><?= 'Список'.$substr.'книг'?></h3>
-
-    <?= GridView::widget([
-        'summary' => false,
-        'dataProvider' => $books,
-        'columns' => array_filter([
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'title',
-            'description:ntext',
-            [
-                'label' => 'Авторы',
-                'attribute' => 'authorNamesFormat',
-                'value' => function($data){
-                    return $data->authorBehavior->objectNamesFormat;
-                }
-            ],
-            [
-                'label' => 'Теги',
-                'attribute' => 'tagNamesFormat',
-                'value' => function($data){
-                    return $data->tagBehavior->objectNamesFormat;
-                }
-            ],
-            //'qr_code',
-
-            !Yii::$app->user->isGuest ? (
-                Yii::$app->user->identity->getId() == $model->id ? ['class' => 'yii\grid\ActionColumn'] : false
-            ) : false,
-
-            // А ВООБЩЕ В СТРОКЕ ВЫШЕ ACTIONCOLUMNНУЖНО ВОТ ТАК ПОМЕНЯТЬ, ЧТОБЫ ССЫЛКИ РАБОТАЛИ
-            // [
-            //     'class' => 'yii\grid\ActionColumn',
-            //     'header' => 'Actions',
-            //     'headerOptions' => ['style' => 'color:#337ab7'],
-            //     'template' => '{view}{update}{delete}',
-            //     'buttons' => [
-            //     'view' => function ($url, $model) {
-            //         return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, [
-            //                     'title' => Yii::t('app', 'lead-view'),
-            //         ]);
-            //     },
+    <h3><?=$q==''?'':'Личный Qr-код'?></h3>
     
-            //     'update' => function ($url, $model) {
-            //         return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
-            //                     'title' => Yii::t('app', 'lead-update'),
-            //         ]);
-            //     },
-            //     'delete' => function ($url, $model) {
-            //         return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
-            //                     'title' => Yii::t('app', 'lead-delete'),
-            //         ]);
-            //     }
-    
-            //     ],
-            //     'urlCreator' => function ($action, $model, $key, $index) {
-            //         if ($action === 'view') {
-            //             $url ='index.php?r=client-login/lead-view&id='.$model->id;
-            //             return $url;
-            //         }
-            //         if ($action === 'update') {
-            //             $url ='index.php?r=client-login/lead-update&id='.$model->id;
-            //             return $url;
-            //         }
-            //         if ($action === 'delete') {
-            //             $url ='index.php?r=client-login/lead-delete&id='.$model->id;
-            //             return $url;
-            //         }
-            //     }
-            // ]
-        ]),
-    ]); ?>
+    <?= $q ?>
  
 </div>
 
